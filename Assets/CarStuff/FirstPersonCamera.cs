1c43455e77448a2571e35cb26ff156af42bd39f2
ben <changb3@rpi.edu>
2015-06-12 14:27:58 -0400

    set default values for car speed so they're not zero, and now you can drive normally

--- Assets/CarStuff/FirstPersonCamera.cs
+++ Assets/CarStuff/FirstPersonCamera.cs
@@ -19,7 +19,7 @@ public class FirstPersonCamera : MonoBehaviour
         
         //Lerp to direction of the camera toward the direction of velocity to simulate realistic head motion
         Vector3 carVel = cameraHandle.root.GetComponent<Rigidbody>().velocity;
-        transform.forward = Vector3.Lerp(transform.forward, carVel, .5f);
+        transform.forward = Vector3.Lerp(transform.forward, carVel, Time.deltaTime);
 
         //if you're not already looking past the point where your head could turn
         if(Vector3.Angle(cameraHandle.forward,transform.forward) < maxHeading)
