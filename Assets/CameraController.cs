c72a188310b8cabcd3a56d99141aa59888b81a3a
Spencer <johnss18@rpi.edu>
2015-06-20 23:08:52 -0400

    car with new camera stuff, currently only works with controller

---
+++ Assets/CameraController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+    public GameObject firstPersonCam;
+    public GameObject thirdPersonCam;
+    public GameObject backUpCam;
+    public GameObject rightSideViewCam;
+    public GameObject leftSideViewCam;
+
+    public GameObject mainCam;
+    public GameObject currentCam;
+	// Use this for initialization
+	void Start () 
+    {
+        firstPersonCam.SetActive(true);
+        thirdPersonCam.SetActive(false);
+        backUpCam.SetActive(false);
+        rightSideViewCam.SetActive(false);
+        leftSideViewCam.SetActive(false);
+
+        mainCam = firstPersonCam;
+        currentCam = firstPersonCam;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if(Input.GetKeyDown(KeyCode.Joystick1Button3))
+        {
+            if (firstPersonCam.activeSelf)
+            {
+                mainCam = thirdPersonCam;
+                ChangeMainCamera(thirdPersonCam);
+            }
+            else
+            {
+                mainCam = firstPersonCam;
+                ChangeMainCamera(firstPersonCam);
+            }
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.Joystick1Button0))
+        {
+            ChangeMainCamera(backUpCam);
+        }
+        if (Input.GetKeyUp(KeyCode.Joystick1Button0))
+        {
+            ChangeMainCamera(mainCam);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button2))
+        {
+            ChangeMainCamera(leftSideViewCam);
+        }
+        if (Input.GetKeyUp(KeyCode.Joystick1Button2))
+        {
+            ChangeMainCamera(mainCam);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button1))
+        {
+            ChangeMainCamera(rightSideViewCam);
+        }
+        if (Input.GetKeyUp(KeyCode.Joystick1Button1))
+        {
+            ChangeMainCamera(mainCam);
+        }
+	}
+
+
+    void ChangeMainCamera(GameObject newCam)
+    {
+        currentCam.SetActive(false);
+        newCam.SetActive(true);
+        currentCam = newCam;
+    }
+    void ChangeTempCamera()
+    {
+
+    }
+}
--- Assets/CarStuff/FirstPersonCamera.cs
+++ Assets/CarStuff/FirstPersonCamera.cs
@@ -3,26 +3,40 @@ using System.Collections;
 
 public class FirstPersonCamera : MonoBehaviour 
 {
-    public Transform cameraHandle;
-
+    public Transform cam;
+    public SimpleCarController carController { get; set; }
     public float maxHeading;
+    public float maxStickHeading;
 
-    void Start()
+    void Awake()
     {
-        transform.position = cameraHandle.position;
-        transform.rotation = cameraHandle.rotation;
+        //assign a ref to our car controller
+        carController = transform.root.GetComponent<SimpleCarController>();
+        //instantiate our first person camera
+        GameObject dummy = (GameObject)Resources.Load("FirstPersonCam");
+        dummy = Instantiate(dummy, transform.position, transform.rotation) as GameObject;
+        cam = dummy.transform;
     }
     void Update()
     {
-        //update
-        transform.position = cameraHandle.position;
-        
-        //Lerp to direction of the camera toward the direction of velocity to simulate realistic head motion
-        Vector3 carVel = cameraHandle.root.GetComponent<Rigidbody>().velocity;
-        transform.forward = Vector3.Lerp(transform.forward, carVel, Time.deltaTime);
+        //update the cam's position and rotation based off our position and rotation
+        cam.position = transform.position;
+        cam.forward = transform.forward;
+
+        //create the camera look direction based on velocity to simulate realistic head motion
+        //then rotate that new vector by the horizontal input of the right stick to give players more look control
+        Vector3 newCamDir = new Vector3();
+        //only do this step if you are travelling forwards or fast enough backwards
+        if (carController.speedInMph > 5 || carController.speedInMph < -15)
+        {
+            newCamDir = transform.root.GetComponent<Rigidbody>().velocity;
+        }
+        newCamDir = Quaternion.AngleAxis(maxStickHeading * Input.GetAxisRaw("2ndStickX"), Vector3.up) * newCamDir;
+        //Lerp to direction of the camera toward the new direction
+        transform.forward = Vector3.Lerp(transform.forward, newCamDir, Time.deltaTime);
 
         //if you're not already looking past the point where your head could turn
-        if(Vector3.Angle(cameraHandle.forward,transform.forward) < maxHeading)
+        if(Vector3.Angle(transform.forward, cam.forward) < maxHeading)
         {
             
         }
